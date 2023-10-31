e88659e195f89abc282a0d2d601951bf45640721
kahzerx <kahzerx@tutanota.com>
2023-10-31 20:58:50 +0100

    xpNoCooldown fix

--- src/main/java/com/kahzerx/carpet/mixins/rule/xpNoCooldown/XpOrbEntityMixin.java
+++ src/main/java/com/kahzerx/carpet/mixins/rule/xpNoCooldown/XpOrbEntityMixin.java
@@ -5,12 +5,13 @@ import net.minecraft.entity.XpOrbEntity;
 import net.minecraft.entity.living.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(XpOrbEntity.class)
 public class XpOrbEntityMixin {
-	@Redirect(method = "onPlayerCollision", at = @At(value = "HEAD"))
-	private void removeDelay(PlayerEntity playerEntity) {
+	@Inject(method = "onPlayerCollision", at = @At(value = "HEAD"))
+	private void removeDelay(PlayerEntity playerEntity, CallbackInfo ci) {
 		if (CarpetSettings.xpNoCooldown) {
 			playerEntity.xpCooldown = 0;
 		}
