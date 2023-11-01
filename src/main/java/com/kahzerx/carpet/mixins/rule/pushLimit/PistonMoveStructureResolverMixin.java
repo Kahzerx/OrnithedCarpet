c5e07465d997d49d6f4d2e60d60467a9ec1eb737
kahzerx <kahzerx@tutanota.com>
2023-11-01 13:51:21 +0100

    piston push limit rule 1.7.10 fix

--- src/main/java/com/kahzerx/carpet/mixins/rule/pushLimit/PistonMoveStructureResolverMixin.java
+++ src/main/java/com/kahzerx/carpet/mixins/rule/pushLimit/PistonMoveStructureResolverMixin.java
@@ -1,15 +1,35 @@
 package com.kahzerx.carpet.mixins.rule.pushLimit;
 
 import com.kahzerx.carpet.CarpetSettings;
+//#if MC<=10710
+//$$ import net.minecraft.block.PistonBaseBlock;
+//#else
 import net.minecraft.block.piston.PistonMoveStructureResolver;
+//#endif
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
+//#if MC<=10710
+//$$ @Mixin(value = PistonBaseBlock.class, priority = 420)
+//#else
 @Mixin(value = PistonMoveStructureResolver.class, priority = 420)
+//#endif
 public class PistonMoveStructureResolverMixin {
+	//#if MC<=10710
+	//$$ @ModifyConstant(method = {"canExtend", "push"}, constant = @Constant(intValue = 12))
+	//$$ private static int pushLimit(int constant) {
+	//#else
 	@ModifyConstant(method = "addColumn", constant = @Constant(intValue = 12), expect = 3)
 	private int pushLimit(int constant) {
+	//#endif
 		return CarpetSettings.pushLimit;
 	}
+
+	//#if MC<=10710
+	//$$ @ModifyConstant(method = {"canExtend", "push"}, constant = @Constant(intValue = 13))
+	//$$ private static int pushLimitIterators(int constant) {
+	//$$ 	return CarpetSettings.pushLimit + 1;
+	//$$ }
+	//#endif
 }
